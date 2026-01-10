09cf4bbaca2cdbafb4740be464a46da3d4a82702
Youssef Khalouk <ykhalouk@c4r5p1.1337.ma>
2026-01-10 16:32:12 +0100

    update

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -1,17 +1,16 @@
 import sys
 
-if __name__ == "__main__":
-    print("=== Command Quest ===")
+print("=== Command Quest ===")
 
-    if len(sys.argv) == 1:
-        print("No arguments provided!")
-        print("Program name:", sys.argv[0])
-        print("Total arguments:", len(sys.argv))
-    else:
-        print("Program name:", sys.argv[0])
-        print("Arguments received:", len(sys.argv) - 1)
+if len(sys.argv) == 1:
+    print("No arguments provided!")
+    print("Program name:", sys.argv[0])
+    print("Total arguments:", len(sys.argv))
+else:
+    print("Program name:", sys.argv[0])
+    print("Arguments received:", len(sys.argv) - 1)
 
-        for i in range(1, len(sys.argv)):
-            print(f"Argument {i}: {sys.argv[i]}")
-            i += 1
-        print("Total arguments:", len(sys.argv))
+    for i in range(1, len(sys.argv)):
+        print(f"Argument {i}: {sys.argv[i]}")
+        i += 1
+    print("Total arguments:", len(sys.argv))
--- ex3/ft_achievement_tracker.py
+++ ex3/ft_achievement_tracker.py
@@ -1,44 +1,41 @@
 
+list1 = ['first_kill', 'level_10', 'treasure_hunter', 'speed_demon']
+list2 = ['first_kill', 'level_10', 'boss_slayer', 'collector']
+list3 = [
+    'level_10', 'treasure_hunter', 'boss_slayer',
+    'speed_demon', 'perfectionist'
+    ]
+alice = set(list1)
+bob = set(list2)
+charlie = set(list3)
 
-if __name__ == "__main__":
+print("=== Achievement Tracker System ===\n")
+print("Player alice achievements: ", alice)
+print("Player bob achievements: ", bob)
+print("Player charlie achievements: ", charlie)
 
-    list1 = ['first_kill', 'level_10', 'treasure_hunter', 'speed_demon']
-    list2 = ['first_kill', 'level_10', 'boss_slayer', 'collector']
-    list3 = [
-        'level_10', 'treasure_hunter', 'boss_slayer',
-        'speed_demon', 'perfectionist'
-        ]
-    alice = set(list1)
-    bob = set(list2)
-    charlie = set(list3)
+print("\n=== Achievement Analytics ===")
+unipue = alice.union(bob, charlie)
+print("All unique achievements: ", unipue)
+print("Total unipue achievements: ", len(unipue))
 
-    print("=== Achievement Tracker System ===\n")
-    print("Player alice achievements: ", alice)
-    print("Player bob achievements: ", bob)
-    print("Player charlie achievements: ", charlie)
+common = alice.intersection(bob, charlie)
+print("\ncommon to all players: ", common)
 
-    print("\n=== Achievement Analytics ===")
-    unipue = alice.union(bob, charlie)
-    print("All unique achievements: ", unipue)
-    print("Total unipue achievements: ", len(unipue))
+rare = (
+    alice.union(bob, charlie)
+    .difference(
+        alice.intersection(bob)
+        .union(alice.intersection(charlie))
+        .union(bob.intersection(charlie))
+    ))
+print("Rare achivements (1 player): ", rare)
 
-    common = alice.intersection(bob, charlie)
-    print("\ncommon to all players: ", common)
+avb = alice.intersection(bob)
+print("\nAlice vs Bob common: ", avb)
 
-    rare = (
-        alice.union(bob, charlie)
-        .difference(
-            alice.intersection(bob)
-            .union(alice.intersection(charlie))
-            .union(bob.intersection(charlie))
-        ))
-    print("Rare achivements (1 player): ", rare)
+alice_unique = alice.difference(bob)
+print("Alice unique: ", alice_unique)
 
-    avb = alice.intersection(bob)
-    print("\nAlice vs Bob common: ", avb)
-
-    alice_unique = alice.difference(bob)
-    print("Alice unique: ", alice_unique)
-
-    bob_unique = bob.difference(alice)
-    print("Bob unique: ", bob_unique)
+bob_unique = bob.difference(alice)
+print("Bob unique: ", bob_unique)
